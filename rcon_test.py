ed49aad49cd285a1eb947da507ca3947ecc5f90f
Lucifer <28718941+kryanide@users.noreply.github.com>
2026-09-18 00:40:56 -0400

    added rcon functionality

---
+++ rcon_test.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from rcon.source import Client
+
+load_dotenv()
+
+HOST = os.getenv("RCON_HOST", "127.0.0.1")
+PORT = int(os.getenv("RCON_PORT", "27015"))
+PASSWORD = os.getenv("RCON_PASSWORD")
+
+with Client(HOST, PORT, passwd=PASSWORD) as client:
+    print(client.run("players"))
